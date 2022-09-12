a6215f153c50f28a4e820e0a567e89bea03156ae
Mohammad Sheikh <mohammad_sheikh@epam.com>
2022-09-12 22:01:13 +0530

    Added new pages

--- src/Layout/loggedInHeader.js
+++ src/Layout/loggedInHeader.js
@@ -11,60 +11,126 @@ export default function LoggedInHeader(props) {
   let isClientPath = userData.type == "client";
 
   return (
-    <header>
-      <div className="container">
-        <div className="tophead border-bottom">
-          <div className="row align-items-center">
-            <div className="col-md-3 col-5 moblogo">
-              <Link to="/">
-                <img src="images/Logo.svg" alt="" />
-              </Link>
-            </div>
-            <div className="col-md-6 col-7">
-              <ul className="tophead-right">
-                <li className="client-loginHead d-none d-sm-block">
-                  <a href="#">
+    <>
+      <header>
+        <div className="container">
+          <div className="tophead border-bottom">
+            <div className="row align-items-center">
+              <div className="col-md-6 col-5 moblogo">
+                <Link to="/">
+                  <img src="images/Logo.svg" alt="" />
+                </Link>
+                {showRegisterButton && (
+                  <>
                     {isClientPath ? (
-                      <img
-                        src="images/pro-headimg.png"
-                        className="pro-headimg"
-                        alt=""
-                      />
+                      <button
+                        type="button"
+                        class="btn btn-login ml-4 d-none d-sm-inline-block"
+                      >
+                        REGISTER AS A SERVICE PROVIDER
+                      </button>
                     ) : (
-                      <img
-                        src="images/service-prohead-img.png"
-                        className="pro-headimg"
-                        alt=""
-                      />
+                      <button
+                        type="button"
+                        class="btn btn-login ml-4 d-none d-sm-inline-block"
+                      >
+                        WANT TO HIRE SOMEONE?
+                      </button>
                     )}
-                    You are logged in as a{" "}
-                    <span>{isClientPath ? "CLIENT" : "SERVICE PROVIDER"}</span>
-                  </a>
-                </li>
-                <li className="user-picHead">
-                  <a href="#">
-                    <span>
-                      <img
-                        srcSet={
-                          (`${userData.profileImageUrl}`,
-                          "images/pro-userpic.jpg")
-                        }
-                        alt=""
-                      />
-                    </span>
-                    <p>Hi, {userData.name} !</p>
-                  </a>
-                </li>
-                <li>
-                  <a onClick={onLogout}>
-                    <img src="images/bell.png" alt="" />
-                  </a>
-                </li>
-              </ul>
+                  </>
+                )}
+              </div>
+              <div className="col-md-6 col-7">
+                <ul className="tophead-right">
+                  <li className="client-loginHead d-none d-sm-block">
+                    <a href="#">
+                      {isClientPath ? (
+                        <img
+                          src="images/pro-headimg.png"
+                          className="pro-headimg"
+                          alt=""
+                        />
+                      ) : (
+                        <img
+                          src="images/service-prohead-img.png"
+                          className="pro-headimg"
+                          alt=""
+                        />
+                      )}
+                      You are logged in as a{" "}
+                      <span>
+                        {isClientPath ? "CLIENT" : "SERVICE PROVIDER"}
+                      </span>
+                    </a>
+                  </li>
+                  <li className="user-picHead">
+                    <a href="#">
+                      <span>
+                        <img
+                          srcSet={
+                            (`${userData.profileImageUrl}`,
+                            "images/pro-userpic.jpg")
+                          }
+                          alt=""
+                        />
+                      </span>
+                      <p>Hi, {userData.name} !</p>
+                    </a>
+                  </li>
+                  <li>
+                    <a onClick={onLogout}>
+                      <img src="images/bell.png" alt="" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {showNav && (
+        <Navbar expand="lg" class="listed-bar">
+          <div class="container">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#" className="active">
+                  <img src="images/blue-status.svg" alt="" />
+                  BROWSE
+                </Nav.Link>
+                <Nav.Link href="#">
+                  <img src="images/note.svg" alt="" />
+                  PROJECTS
+                </Nav.Link>
+                <Nav.Link href="#">
+                  <img src="images/gray-cal.svg" alt="" />
+                  BOOKINGS
+                </Nav.Link>
+                <Nav.Link href="#">
+                  <img src="images/dollar.svg" alt="" />
+                  EARNINGS
+                </Nav.Link>
+                <Nav.Link href="#">
+                  <img src="images/mgs.svg" alt="" />
+                  SUPPORT
+                </Nav.Link>
+                <Nav.Link href="#">
+                  <img src="images/setting.svg" alt="" />
+                  ACCOUNT
+                </Nav.Link>
+                <Nav.Link href="#">
+                  <img src="images/archive.svg" alt="" />
+                  SAVED
+                </Nav.Link>
+                <Nav.Link href="#">
+                  <img src="images/trend.svg" alt="" />
+                  REPORTS
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        </Navbar>
+      )}
+    </>
   );
 }
